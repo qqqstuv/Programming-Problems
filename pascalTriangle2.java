b128b496c1a732a3a6e3d28c1a52d0446ee0848b
Duke Nguyen <nguyentranduc1@gmail.com>
2017-06-10 16:52:25 -0700

    Add pascal 2

---
+++ pascalTriangle2.java
@@ -0,0 +1,16 @@
+/*
+Source: https://leetcode.com/problems/pascals-triangle-ii/#/solutions
+Topic: @Array, @Math
+*/
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = 0;i<rowIndex+1;i++) {
+        		res.add(1);
+        		for(int j=i-1;j>0;j--) {
+        			res.set(j, res.get(j-1)+res.get(j));
+        		}
+        }
+        return res;
+    }
+}
