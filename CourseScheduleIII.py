d7b592becf7d78f468e3498179a78882d9b343c6
Duke Nguyen <nguyentranduc1@gmail.com>
2017-06-25 17:01:10 -0700

    Create CourseScheduleIII.py

---
+++ CourseScheduleIII.py
@@ -0,0 +1,9 @@
+def scheduleCourse(self, A):
+    pq = []
+    start = 0
+    for t, end in sorted(A, key = lambda (t, end): end):
+        start += t
+        heapq.heappush(pq, -t)
+        while start > end:
+            start += heapq.heappop(pq)
+    return len(pq)
