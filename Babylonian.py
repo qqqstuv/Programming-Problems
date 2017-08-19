6cbdef170c1b1a17fa596be21002c87e93a69d1b
Duke Nguyen <nguyentranduc1@gmail.com>
2017-08-19 14:34:51 -0700

    Add files via upload

---
+++ Babylonian.py
@@ -0,0 +1,24 @@
+
+import fileinput
+
+def Babylonian(numList):
+    base = 0
+    exponent = len(numList) - 1
+    for i in numList:
+        if i != '':
+            i = int(i)
+            base += i * pow(60,exponent)
+        exponent -= 1
+
+    print(base)
+
+
+
+
+
+skip = True
+for line in fileinput.input():
+    if not skip:
+        Babylonian(line.strip().split(","))
+    else:
+        skip = False
