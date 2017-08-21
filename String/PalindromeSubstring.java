0e0d99227864db97b8c23d0d7efdf905a3c64c90
Nguyen, Duc <dnguyen@abebooks.com>
2017-08-21 14:40:23 -0700

    add files

---
+++ String/PalindromeSubstring.java
@@ -0,0 +1,35 @@
+
+
+
+
+
+import java.io.*;
+import java.util.*;
+
+class PalindromeSubstring {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	    
+	    String line;
+	    while ((line = reader.readLine()) != null) {
+	        System.out.println(line);
+	    	for(int i = 0; i < length - 1; i++){ // check for small test case
+	    		if(line.charAt(i + 1) == line.charAt(i)){ // left one and right one maybe
+	    			int offset = Integer.min(i, length - i);
+	    			while(offset == 0){
+	    				if (i - ) {
+	    					
+	    				}
+	    			}
+	    		}
+	    		if(line.charAt(i + 1) == line.charAt(i)){
+
+	    		}
+			}
+	    }
+
+
+
+	}
+}
