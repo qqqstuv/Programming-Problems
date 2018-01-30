45be2d769c5f380faa0b1ad0405b83b7d211e103
Duke Nguyen <nguyentranduc1@gmail.com>
2018-01-30 13:05:31 -0800

    Change LCS typos

--- String/LCS/lcs.py
+++ String/LCS/lcs.py
@@ -9,17 +9,19 @@ def LCS(A,B):
 	else:
 		value = ""
 		if len(A) == 0 or len(B) == 0:
-			return 0
+			return ""
 		elif A[-1] == B[-1]: # same last character
 			value = LCS(A[:-1],B[:-1]) + A[-1]
 		else: # different last character
 			dec_A = LCS(A[:-1], B)
 			dec_B = LCS(A, B[:-1])
-			value = (len(dec_A) > (dec_B)) ? dec_A : dec_B
+			value = dec_A if (len(dec_A) > len(dec_B))  else dec_B
 		memo[key] = value
 		return value
 
 
-string_A = "ABCDCDBACDB"
-string_B = "ABCDBACBDCBA"
+string_A = "ABCBDAB"
+string_B = "BDCABA"
+
+print (LCS(string_A, string_B))
 
